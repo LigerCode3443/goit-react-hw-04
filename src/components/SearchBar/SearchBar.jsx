fa2554654c089a254851d87efe1e666019955abe
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-06-19 13:46:31 +0300

    +validation-form

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,24 @@
 import { useForm } from "react-hook-form";
 import s from "./SearchBar.module.css";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const searchSchema = Yup.object({
+  query: Yup.string()
+    .min(2, "For a short name")
+    .max(30, "For a long name")
+    .required("Empty field"),
+});
 
 export const SearchBar = ({ setQuery }) => {
-  const { register, handleSubmit, reset } = useForm({});
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(searchSchema),
+  });
   const onSubmit = (data) => {
     setQuery(data.query);
     reset();
@@ -17,6 +33,9 @@ export const SearchBar = ({ setQuery }) => {
           placeholder="Search..."
           className={s.input}
         />
+        {errors.query && (
+          <div className={s.error_valid}>{errors.query.message}</div>
+        )}
         <button className="btn">Search</button>
       </form>
     </header>
